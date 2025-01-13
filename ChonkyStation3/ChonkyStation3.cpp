8c7ae77af2cd849a7cc318c542ceaddfa2ebda08
wheremyfoodat <44909372+wheremyfoodat@users.noreply.github.com>
2025-01-13 03:09:25 +0200

    Prefer dGPU instead of iGPU

--- ChonkyStation3/ChonkyStation3.cpp
+++ ChonkyStation3/ChonkyStation3.cpp
@@ -1,9 +1,19 @@
-﻿#include <iostream>
+#include <iostream>
 #include <string>
 #include <format>
 #include <SDL.h>
 #include "PlayStation3.hpp"
 
+#ifdef _WIN32
+#include <windows.h>
+
+// Gently ask to use the discrete Nvidia/AMD GPU if possible instead of
+// integrated graphics
+extern "C" {
+__declspec(dllexport) DWORD NvOptimusEnablement = 1;
+__declspec(dllexport) DWORD AmdPowerXpressRequestHighPerformance = 1;
+}
+#endif
 
 static constexpr double MS_PER_FRAME = 1000.0 / 60.0;
 
